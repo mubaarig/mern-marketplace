992c05fcc67fec60958b5f090920e4e27b3947b5
Mubarig Cisman <mubo1@gmail.com>
2022-05-29 23:19:13 +0200

    Frontend With React

---
+++ frontend/src/data.js
@@ -0,0 +1,83 @@
+const data = {
+    products:[
+        {
+            id: 1,
+            name: "Padomi Skirt",
+            category: "Skirts",
+            image : "/images/pd1.jpg",
+            price:  219.90 ,
+            brand: "Padomi",
+            rating: 4.6,
+            numOfReviews: 12,
+            description: "High quality product"
+        },
+        {
+            id: 2,
+            name: "Padomi Skirt",
+            category: "Skirts",
+            image : "images/pd2.jpg",
+            price:  329.90 ,
+            brand: "Padomi",
+            rating: 4.3,
+            numOfReviews: 19,
+            description: "High quality product"
+        },
+        {
+            id: 3,
+            name: "Elie Skirt",
+            category: "Skirts",
+            image : "images/pd3.webp",
+            price:  329.90 ,
+            brand: "Padomi",
+            rating: 4.3,
+            numOfReviews: 19,
+            description: "High quality product"
+        },
+        {
+            id: 4,
+            name: "Padomi Skirt",
+            category: "Skirts",
+            image : "images/pd4.jpg",
+            price:  198.90 ,
+            brand: "Padomi",
+            rating: 4.3,
+            numOfReviews: 19,
+            description: "High quality product"
+        },
+        {
+            id: 5,
+            name: "Lamadi Skirt",
+            category: "Skirts",
+            image : "images/pd5.webp",
+            price:  329.90 ,
+            brand: "Lamadi",
+            rating: 4.3,
+            numOfReviews: 19,
+            description: "High quality product"
+        },
+        {
+            id: 6,
+            name: "Adalio Skirt",
+            category: "Skirts",
+            image : "images/pd4.jpg",
+            price:  267.90 ,
+            brand: "Adali0",
+            rating: 5,
+            numOfReviews: 9,
+            description: "High quality product"
+        },
+        {
+            id: 7,
+            name: "Lacosto Skirt",
+            category: "Skirts",
+            image : "images/pd1.jpg",
+            price:  390.90 ,
+            brand: "Lacosto",
+            rating: 4.9,
+            numOfReviews: 19,
+            description: "High quality product"
+        },
+    ],
+};
+
+export default data;
