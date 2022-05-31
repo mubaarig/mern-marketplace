b05203272897c771acf74aa47237a879b130dbbf
Mubarig Cisman <mubo1@gmail.com>
2022-05-31 15:28:35 +0200

    Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
+// import {
+//     BrowserRouter as Router,
+//     Routes, 
+//     Route,
+//     Link
+//     } from 'react-router-dom';
+    import {
+        BrowserRouter,
+        Routes,
+        Route,
+        Link
+    } from 'react-router-dom';
+    
 import Products from './components/products/Products';
 
 function App() {
     return (
-        <div className="grid-container">
-            <header className="row">
-                <div><a className="brand" href="/">UNION</a></div>
-                <div>
-                    <a href="/cart">Cart</a>
-                    <a href="/signin">Sign In</a>
-                </div>
-            </header>
-            <main>
-               <Products/>
-            </main>
-            <footer className="row center">
-                All rights reserved
-            </footer>
-        </div>
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="row">
+                    <div><a className="brand" href="/">UNION</a></div>
+                    <div>
+                        <a href="/cart">Cart</a>
+                        <a href="/signin">Sign In</a>
+                    </div>
+                </header>
+                <main><Products /></main>
+                <footer className="row center">
+                    All rights reserved
+                </footer>
+            </div>
+        </BrowserRouter>
     );
 }
 
